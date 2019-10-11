8b3ccc9955e990e6f41583a7310b6f724c6d94bc
= <=>
2019-10-11 20:50:54 +0700

    index.c

--- index.c
+++ index.c
@@ -14,6 +14,48 @@ void read_input(char *inputBuffer, int length)
     read(STDIN_FILENO, inputBuffer, MAX_LINE);
 }
 
+//Ham thay the cua strsep ben Linux
+//Gia tri tra ve: Chuoi ki tu duoc chia
+//stringp: Chuoi dau vao
+//delim: cac ki tu de phan biet chuoi can chia
+char* mystrsep(char** stringp, const char* delim)
+{
+	char* start = *stringp;
+	char* p;
+
+	p = (start != NULL) ? strpbrk(start, delim) : NULL;
+
+	if (p == NULL)
+	{
+		*stringp = NULL;
+	}
+	else
+	{
+		*p = '\0';
+		*stringp = p + 1;
+	}
+
+	return start;
+}
+
+//Ham chia chuoi inputBuffer thanh cac chuoi trong mang args
+//Gia tri tra ve: Khong co
+//inputBuffer: chuoi dau tien nhan vao
+//args: mang cac chuoi se duoc chia ra
+void divide_args(char *inputBuffer, char **args)
+{
+	char *found;
+	int i;
+	i = 0;
+
+	//Su dung found de lay tung chuoi ki tu tu chuoi inputBuffer va sau do gan vao args 
+	while ((found = mystrsep(&inputBuffer, " ")) != NULL) //mystrsep co the thay the bang strsep khi su dung linux voi cac doi so van giu nguyen
+	{
+		args[i] = found;
+		i++;
+	}
+}
+
 void loop()
 {
     clrscr();
@@ -27,6 +69,7 @@ void loop()
         fflush(stdout);
         read_input(inputBuffer, MAX_LINE);
         fflush(stdin);
+		divide_args(inputBuffer, args);
     }
 }
 
